c10a58da7003ca35119f169b51b39b07cbcd8f19
Temistocles17 <44779529+Temistocles17@users.noreply.github.com>
2025-12-01 21:48:47 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+const myImage = document.querySelector("img");
+
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Compass.jpg") {
+    myImage.setAttribute("src", "images/compass2.webp");
+  } else {
+    myImage.setAttribute("src", "images/Compass.jpg");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
